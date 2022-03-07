62ba9c4e317da8984a03cf56497edad709918277
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-07 22:02:55 +0100

    Giving style to the snippet component

--- src/components/SnippetCollectionCard/SnippetCollectionCard.tsx
+++ src/components/SnippetCollectionCard/SnippetCollectionCard.tsx
@@ -1,6 +1,6 @@
 const SnippetCollectionCard: React.FC = () => {
   return (
-    <div className="bg-yellowSpace flex flex-row justify-between h-9 rounded-xl pl-6 pr-6 items-center ml-6 mr-6">
+    <div className="bg-yellowSpace flex flex-row justify-between h-16 rounded-xl pl-6 pr-6 items-center mt-6">
       <h3>Ingreation components</h3>
       <span>JS</span>
     </div>
