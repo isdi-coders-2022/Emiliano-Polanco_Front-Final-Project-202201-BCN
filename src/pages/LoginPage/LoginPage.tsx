2c4aac32ad65336618ef230086eb64885663e694
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-12 20:22:03 +0100

    Giving some style to the form login component

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,11 +1,11 @@
-import LoginForm from "../../components/GenericCard/Forms/LoginForm/LoginForm";
+import LoginForm from "../../components/Forms/LoginForm/LoginForm";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 
 const LoginPage = () => {
   return (
     <>
       <NavigationBar />
-      <div className=" h-screen flex justify-center items-center bg-orangeSpace">
+      <div className=" h-screen flex justify-center items-center ">
         <LoginForm />
       </div>
     </>
