feae6c17e6b99985fc0c6c695af432ac921c8a4c
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-12 11:52:44 +0100

    Making the testing fo the GenericCardEdit component

--- src/components/GenericCard/GenericCard.test.tsx
+++ src/components/GenericCard/GenericCard.test.tsx
@@ -3,48 +3,50 @@ import { BrowserRouter } from "react-router-dom";
 import userInterface from "../../redux/interfaces/userInterface";
 import GenericCard from "./GenericCard";
 
+const guestUser: userInterface = {
+  name: "New exited User",
+  lastname: "Happy",
+  username: "",
+  email: "",
+  password: "",
+  pet: "",
+  membership: "",
+  scoreHistoryWpm: [],
+  snippetsJavaScript: [],
+  snippetsPhyton: [],
+  snippetsCsharp: [],
+  scoreHistoryAccuracy: [],
+  scoreHistoryPerCharacter: [], //ojuuu ehhh que eesto esta mal y hay que revisarlo
+  snippetsTypeScript: [],
+  snippetsCollection: [
+    {
+      _id: "62273e455e1654cd7ad00941",
+      language: "JavaScript",
+      textCode:
+        // eslint-disable-next-line no-template-curly-in-string
+        'const startServer = (app, port) =>\r\n  new Promise((resolve, reject) => {\r\n    const server = app.listen(port, () => {\r\n      debug(`The server it\'s up in http://localhost:${port}`);\r\n      resolve();\r\n    });\r\n\r\n    server.on("error", (error) => {\r\n      debug(`Oh no the server couldnt start ${error.message}`);\r\n      reject();\r\n    });\r\n  });\r\n\r\nmodule.exports = startServer;',
+      title: "start an express server",
+    },
+    {
+      _id: "62273ed65e1654cd7ad00943",
+      language: "JavaScript",
+      textCode:
+        'require("dotenv").config();\r\nconst cors = require("cors");\r\nconst express = require("express");\r\nconst morgan = require("morgan");\r\nconst { notFoundError, internalServerError } = require("./middlewares/errors");\r\nconst loginRouter = require("./routers/loginRouter");\r\nconst robotsRouter = require("./routers/robotsRouter");\r\n\r\nconst app = express();\r\n\r\napp.use(cors());\r\napp.use(morgan("dev"));\r\napp.use(express.json());\r\napp.use("/robots", robotsRouter);\r\napp.use("/login", loginRouter);',
+      title: "normal chain of middlewares",
+    },
+    {
+      _id: "6228b158611c53bc237d8c09",
+      language: "TypeScript",
+      textCode:
+        'function padLeft(padding: number | string, input: string) {\r\n  if (typeof padding === "number") {\r\n    return " ".repeat(padding) + input;\r\n  }\r\n  return padding + input;\r\n}',
+      title: "basics of typeScript",
+    },
+  ],
+};
+
 describe("Given a GenericCard component", () => {
   describe("When it's rendered", () => {
     test("Then it should return", () => {
-      const guestUser: userInterface = {
-        name: "New exited User",
-        lastname: "Happy",
-        username: "",
-        email: "",
-        password: "",
-        pet: "",
-        membership: "",
-        scoreHistoryWpm: [],
-        snippetsJavaScript: [],
-        snippetsPhyton: [],
-        snippetsCsharp: [],
-        scoreHistoryAccuracy: [],
-        scoreHistoryPerCharacter: [], //ojuuu ehhh que eesto esta mal y hay que revisarlo
-        snippetsTypeScript: [],
-        snippetsCollection: [
-          {
-            _id: "62273e455e1654cd7ad00941",
-            language: "JavaScript",
-            textCode:
-              'const startServer = (app, port) =>\r\n  new Promise((resolve, reject) => {\r\n    const server = app.listen(port, () => {\r\n      debug(`The server it\'s up in http://localhost:${port}`);\r\n      resolve();\r\n    });\r\n\r\n    server.on("error", (error) => {\r\n      debug(`Oh no the server couldnt start ${error.message}`);\r\n      reject();\r\n    });\r\n  });\r\n\r\nmodule.exports = startServer;',
-            title: "start an express server",
-          },
-          {
-            _id: "62273ed65e1654cd7ad00943",
-            language: "JavaScript",
-            textCode:
-              'require("dotenv").config();\r\nconst cors = require("cors");\r\nconst express = require("express");\r\nconst morgan = require("morgan");\r\nconst { notFoundError, internalServerError } = require("./middlewares/errors");\r\nconst loginRouter = require("./routers/loginRouter");\r\nconst robotsRouter = require("./routers/robotsRouter");\r\n\r\nconst app = express();\r\n\r\napp.use(cors());\r\napp.use(morgan("dev"));\r\napp.use(express.json());\r\napp.use("/robots", robotsRouter);\r\napp.use("/login", loginRouter);',
-            title: "normal chain of middlewares",
-          },
-          {
-            _id: "6228b158611c53bc237d8c09",
-            language: "TypeScript",
-            textCode:
-              'function padLeft(padding: number | string, input: string) {\r\n  if (typeof padding === "number") {\r\n    return " ".repeat(padding) + input;\r\n  }\r\n  return padding + input;\r\n}',
-            title: "basics of typeScript",
-          },
-        ],
-      };
       render(
         <BrowserRouter>
           <GenericCard userData={guestUser} />
@@ -63,6 +65,16 @@ describe("Given a GenericCard component", () => {
       expect(title1).toBeInTheDocument();
       expect(title2).toBeInTheDocument();
       expect(title3).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered and someone click the edit button", () => {
+    test("Then it should change its state", () => {
+      render(
+        <BrowserRouter>
+          <GenericCard userData={guestUser} />
+        </BrowserRouter>
+      );
     });
   });
 });
--- src/components/GenericCard/GenericCardEdit.test.tsx
+++ src/components/GenericCard/GenericCardEdit.test.tsx
@@ -1,10 +1,23 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
 import GenericCardEdit from "./GenericCardEdit";
 
-describe("Given a GenericCard component", () => {
+describe("Given a GenericCardEdit component", () => {
   describe("When it's rendered", () => {
     test("Then it should", () => {
-      // render(<GenericCardEdit />);
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <GenericCardEdit />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const title = screen.getByText("Title");
+
+      expect(title).toBeInTheDocument();
     });
   });
 });
