4f17042bb1c18f117a3781c1d65a4042093636b8
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-13 15:36:30 +0100

    Refactoring test NavigationBar

--- src/components/NavigationBar/NavigationBar.test.tsx
+++ src/components/NavigationBar/NavigationBar.test.tsx
@@ -1,18 +1,27 @@
 import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import NavigationBar from "./NavigationBar";
+import store from "../../redux/store/store";
 
 describe("Given a NavegationBar component", () => {
   describe("When it's rendered", () => {
     test("Then it should display the a login text", () => {
       render(
-        <BrowserRouter>
-          <NavigationBar />
-        </BrowserRouter>
+        <Provider store={store}>
+          <BrowserRouter>
+            <NavigationBar />
+          </BrowserRouter>
+        </Provider>
       );
       const navigationHome: object = screen.getByRole("link", {
         name: "Login",
       });
+      const aboutElement: object = screen.getByRole("link", {
+        name: "About",
+      });
+
+      expect(aboutElement).toBeInTheDocument();
 
       expect(navigationHome).toBeInTheDocument();
     });
