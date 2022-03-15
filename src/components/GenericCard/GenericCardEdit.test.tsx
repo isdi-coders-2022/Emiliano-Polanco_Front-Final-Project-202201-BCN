c42d7dd79d632cab51ee7331a3523320de360d59
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-15 13:13:58 +0100

    Testing the delete and submite button

--- src/components/GenericCard/GenericCardEdit.test.tsx
+++ src/components/GenericCard/GenericCardEdit.test.tsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "../../redux/store/store";
 import GenericCardEdit from "./GenericCardEdit";
+
+const mockNavigate = jest.fn();
+const mockDispatch = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockNavigate,
+}));
 
 describe("Given a GenericCardEdit component", () => {
   describe("When it's rendered", () => {
@@ -18,6 +27,48 @@ describe("Given a GenericCardEdit component", () => {
       const title = screen.getByText("Title");
 
       expect(title).toBeInTheDocument();
+    });
+  });
+
+  describe("When it's rendered and a user clicks the Delete button", () => {
+    test("Then it should wait and do an action", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <GenericCardEdit />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      jest.useFakeTimers();
+      jest.spyOn(global, "setTimeout");
+
+      const submitButton = screen.getByRole("button", { name: "Delete" });
+
+      userEvent.click(submitButton);
+
+      expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 500);
+    });
+  });
+
+  describe("When it's rendered and a user clicks the Done button", () => {
+    test("Then it Formik should handle the rest", () => {
+      jest.mock("react-redux", () => ({
+        ...jest.requireActual,
+        useDispatch: () => mockDispatch,
+      }));
+
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <GenericCardEdit />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const submitButton = screen.getByRole("button", { name: "Done" });
+
+      userEvent.click(submitButton);
     });
   });
 });
