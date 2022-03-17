e73721ab6127e8df8c598656e6a7ff2b0e462773
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-17 13:32:33 +0100

    Creating the actionType and actionCreator of the thunk

--- src/redux/actions/actionTypes/actionTypesSnippets.ts
+++ src/redux/actions/actionTypes/actionTypesSnippets.ts
@@ -1,8 +1,10 @@
 interface typesSnippet {
   createSnippet: string;
+  loadGameSnippet: string;
 }
 const actionTypesSnippets: typesSnippet = {
   createSnippet: "create a snippet for the global collection of snippets",
+  loadGameSnippet: "Load an snippet to the game state",
 };
 
 export default actionTypesSnippets;
--- src/redux/thunks/gameThunk.ts
+++ src/redux/thunks/gameThunk.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 import { Dispatch } from "react";
 import { Action } from "redux";
+import { updateCurrentGameActionCreator } from "../actions/actionCreators/actionCreatorGame";
 const apiUrl = process.env.REACT_APP_API_URL;
 export const getSnippetGame =
   (idSnippet: string) => async (dispatch: Dispatch<Action>) => {
     try {
       const response = await axios.get(`${apiUrl}snippet?id=${idSnippet}`);
       const snippet = response.data;
-      dispatch(updateCurrentGame(snippet));
+      dispatch(updateCurrentGameActionCreator(snippet));
     } catch (error) {
       //dispatch error
     }
