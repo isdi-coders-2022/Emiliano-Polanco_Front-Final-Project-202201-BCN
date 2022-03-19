286810c21a4170f9167a463832289e86b24cb6e5
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-19 15:47:00 +0100

    Fixing variable

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import RegisterForm from "../../components/Forms/RegisterForm/RegisterForm";
-import userInterface from "../../redux/interfaces/userInterface";
 import { RootState } from "../../redux/reducers";
 
 const RegisterPage = () => {
