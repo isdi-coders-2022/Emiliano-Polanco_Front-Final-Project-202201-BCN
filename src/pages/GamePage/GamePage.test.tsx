7f642ca3a58a489a8c8bf9cabc96710dae14f608
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-03-23 10:24:26 +0100

    GamePage testing

---
+++ src/pages/GamePage/GamePage.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "../../redux/store/store";
+import GamePage from "./GamePage";
+
+describe("Given a HomePage component", () => {
+  describe("When it get's rendered", () => {
+    test("Then it should display a title", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <GamePage></GamePage>
+          </BrowserRouter>
+        </Provider>
+      );
+      const text = screen.getByText("Start typing");
+
+      expect(text).toBeInTheDocument();
+    });
+  });
+});
